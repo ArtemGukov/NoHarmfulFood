5e6b7c16771773300a109bb794e70dcf299901a9
Artem Gukov <artem.gukov@gmail.com>
2019-04-10 23:58:22 +0300

    Initial Commit

---
+++ NoHarmfulFood/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NoHarmfulFood
+//
+//  Created by Артем on 10/04/2019.
+//  Copyright © 2019 Gukov.space. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
